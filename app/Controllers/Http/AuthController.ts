69b7a98e2a1e446d77c246bdc758e3ee54839f4f
Christian-M-Silva <tininjacms@gmail.com>
2023-03-22 06:12:24 -0300

    feito a logica do login e logout

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,7 +1,15 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AuthController {
-  public async store({}: HttpContextContract) {}
+  public async store({request, auth}: HttpContextContract) {
+    const {password, entity} = request.all()
+    const token = await auth.use('api').attempt(entity, password, {
+      expiresIn: '30 days',
+    })
+    return token
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({auth}: HttpContextContract) {
+    await auth.use('api').logout()
+  }
 }
